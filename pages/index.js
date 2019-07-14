d9fd02de45679c53c7cef30ac0d2055b44490367
Ruangyot Thanmongkol <ruangyotth@gmail.com>
2019-07-14 21:04:30 +0700

    Add style for post

--- pages/index.js
+++ pages/index.js
@@ -1,71 +1,108 @@
-import Link from 'next/link'
 import axios from 'axios'
+import styled from 'styled-components'
 import _ from 'lodash'
+import Pagination from 'react-paginate';
+//import css
+import '../css/reset.css'
+import '../css/styles.css'
 
 const api = axios.create({
   baseURL: 'http://maqe.github.io/json'
 })
 
-const PostLink = ({ post }) => (
-    <li key={post.id}>
-        <Link as={`/p/${post.id}`} href={`/post?id=${post.id}`}>
-            <a>{post.title}</a>
-        </Link>
-        <style jsx>{`
-      li {
-        list-style: none;
-        margin: 5px 0;
-      }
+const Wrapper = styled.div`
+  margin: 0 auto;
+  width: 1200px;
+  padding: 10px;
+`
+const Post = styled.div`
+  border: 1px solid #dddddd;
+  box-shadow: 2px 2px 5px #ececec;
+  padding: 10px;
+  margin-bottom: 10px;
+  background-color: #f0f0f0;
+  display: flex;
+  position: relative;
+  &:nth-child(2n+1) {
+    background: #ffffff;
+  }
+`
+const PostImage = styled.img`
+  display: block;
+  width: auto;
+  height: 140px;
+  margin-right: 10px;
+  float: left;
+`
 
-      a {
-        text-decoration: none;
-        color: blue;
-        font-family: 'Arial';
-      }
+const PostHeader = styled.h3`
+  font: bold 1.2em;
+  color: #000;
+`
+const PostDetail = styled.div`
+  border-right: 1px solid #dddddd;
+  width: 85%;
+`
+const PostBody = styled.p`
+  opacity: 0.8;
+  font-size: 0.85em;
+`
+const Author = styled.div`
+  text-align: center;
+  width: 15%;
+`
+const AvatarImg = styled.img`
+  width: 80px;
+  border-radius: 60px;
+  margin-bottom: 10px;
+`
+const AuthorName = styled.span`
+  color: #c83430;
+  font-size: 0.85em;
+  display: block;
+  margin-bottom: 10px;
+`
+const AuthorPlace = styled.span`
+  font-size: 0.85em;
+  display: block;
+`
+const AuthorRole = styled.span`
+  font-size: 0.75em;
+  display: block;
+  margin-bottom: 10px;
+`
 
-      a:hover {
-        opacity: 0.6;
-      }
-    `}</style>
-    </li>
+const PostItem = ({ post }) => (
+    <Post>
+      <PostDetail>
+        <PostImage src={post.image_url}></PostImage>
+        <PostHeader>{post.title}</PostHeader>
+        <PostBody>{post.body}</PostBody>
+      </PostDetail>
+      <Author>
+        <AvatarImg src={post.avatar_url}/>
+        <AuthorName>{post.name}</AuthorName>
+        <AuthorRole>{post.role}</AuthorRole>
+        <AuthorPlace><i class=""></i>{post.place}</AuthorPlace>
+      </Author>
+    </Post>
 );
 
 const Index = props => {
   console.log(props.posts)
   
   return (
-    <div>
+    <Wrapper>
       <h1>MAQA Forums</h1>
-      <ul>
+      <h2>Subtitle</h2>
+      <h3>Posts</h3>
+      <div>
         {props.posts.map(post => (
-          <PostLink key={post.id} post={post}></PostLink>
+          <PostItem key={post.id} post={post}></PostItem>
         ))}
-      </ul>
-      <style jsx>{`
-          h1,
-          a {
-            font-family: 'Arial';
-          }
-
-          ul {
-            padding: 0;
-          }
-
-          li {
-            list-style: none;
-            margin: 5px 0;
-          }
-
-          a {
-            text-decoration: none;
-            color: blue;
-          }
-
-          a:hover {
-            opacity: 0.6;
-          }
-      `}</style>
-    </div>
+        <Pagination></Pagination>
+      </div>
+    </Wrapper>
   )
 }
 
@@ -73,8 +110,9 @@ Index.getInitialProps = async function() {
   const res = await api.get('/posts.json')
   const authors = await api.get('/authors.json')
   const posts = res.data
+  const size = 8
   return {
-    posts: posts.map((post, i) => {
+    posts: posts.slice(0, size).map((post, i) => {
         const author = _.filter(authors.data, _.matches({ 'id' : post.author_id }))
         console.log(author)
         return (
